ed55938a2b980b7ed5df7dec933c46ab0c889da3
andrew <twigonometry2@gmail.com>
2024-01-26 09:48:19 -0500

    Fixed misc grammatical errors and clarified some wording; also introduced a couple of new errors frequently used by native speakers in dialogue

--- Assets/Scripts/Objects/NPCS/Helen.cs
+++ Assets/Scripts/Objects/NPCS/Helen.cs
@@ -12,8 +12,8 @@ public class Helen : NPC
         base.Init();
         var startDialogue = new List<string> {
             "James! I've been waiting for you. Your dad asked me to take care of you for the summer.",
-            "We are going to my farm kid! Lots of cool stuff to do there!",
-            "Pack your things, we are leaving today.",
+            "We're going to the farm, kid! Lots of cool stuff to do there!",
+            "Pack your things - we leave today.",
             "Go through the <color=#ffee00>yellow</color> door to the left when you are ready.",
             "Use your 'Right Arrow' key for the <color=#1747c2>blue</color> platform.{SecondDialogue}"
         };
@@ -26,7 +26,7 @@ public class Helen : NPC
     public void SecondDialogue () {
         _doorGO.SetActive(true);
         var secondDialogue = new List<string> {
-            "Go through the <color=#ffee00>yellow</color> the door to the left when you are ready.",
+            "Go through the <color=#ffee00>yellow</color> door to the left when you are ready.",
             "Use your 'Right Arrow' key for the <color=#1747c2>blue</color> platform."
         };
         dialoguesDictionary.Add("secondDialogue", secondDialogue);
